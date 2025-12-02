ee2437c22f6990f00d92e14ec1722c43ea6d6466
Adrian Cobos Franco <cobos.franco.adrian@iescamas.es>
2025-12-02 21:00:19 +0100

    se hicieron los test de cuentacorriente

--- Ejercicios/ActividadesU7/maven/Unidad7/src/test/java/com/cuentacorriente/app/CuentaCorrienteTest.java
+++ Ejercicios/ActividadesU7/maven/Unidad7/src/test/java/com/cuentacorriente/app/CuentaCorrienteTest.java
@@ -12,8 +12,29 @@ class CuentaCorrienteTest {
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testIngresarSaldo() {
+		CuentaCorriente cuenta = new CuentaCorriente("Adrian", "99999999A");
+		double cantidad = 67;
+		cuenta.IngresarDinero(cantidad);
+		assertEquals(cuenta.getSaldo(), cantidad);
+	}
+	
+	@Test
+	void testSetLimit() {
+		CuentaCorriente cuenta = new CuentaCorriente("Adrian", "99999999A");
+		double cantidad = -67;
+		double cantidadEsperada = cantidad;
+		cuenta.setLimitDescub(cantidad);
+		assertEquals(cuenta.getLimitDescub(),  cantidadEsperada);
+	}
+	
+	@Test
+	void testSacarDinero() {
+		CuentaCorriente cuenta = new CuentaCorriente(100);
+		double cantidadSacada =  43;
+		double cantidadEsperada = cuenta.getSaldo() - cantidadSacada;
+		cuenta.SacarDinero(cantidadSacada);
+		assertEquals(cuenta.getSaldo(), cantidadEsperada);
 	}
 
 }

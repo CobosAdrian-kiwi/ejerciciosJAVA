b06b7b3db18b56164c48d3a6c72273aceff8b04e
Adrian Cobos Franco <cobos.franco.adrian@iescamas.es>
2026-01-20 20:58:33 +0100

    Se hizo el ejercicio 8 Sintonizador digital de emisoras FM (una clase con un Main para comprobar)

---
+++ TDD/AppFM/src/main/java/com/fm/App.java
@@ -0,0 +1,26 @@
+package com.fm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       SintoniDigiFM fm = new SintoniDigiFM();
+       
+       fm.Display();
+       
+       fm.FrecuenUP();
+       
+       fm.Display();
+       
+       fm.FrecuenDOWN();
+       
+       fm.Display();
+       
+       fm.FrecuenDOWN();
+       fm.Display();
+    }
+}
---
+++ TDD/AppFM/src/main/java/com/fm/SintoniDigiFM.java
@@ -0,0 +1,33 @@
+package com.fm;
+
+public class SintoniDigiFM {
+	private double frecuencia;
+	private final double MAX_FRECUENCIA = 108.0;
+	private  final double MIN_FRECUENCIA = 80.0;
+	
+	//Constructores
+	public SintoniDigiFM() {
+		this.frecuencia = 80.0;
+	}
+	
+	//Métodos
+	public void FrecuenUP() {
+		if (frecuencia < MAX_FRECUENCIA) {
+			frecuencia += 0.5;
+		}else if(frecuencia == MAX_FRECUENCIA) {
+			frecuencia = MIN_FRECUENCIA;
+		}
+	}
+	
+	public void FrecuenDOWN() {
+		if (frecuencia > MIN_FRECUENCIA) {
+			frecuencia -= 0.5;
+		}else if (frecuencia == MIN_FRECUENCIA) {
+			frecuencia = MAX_FRECUENCIA;
+		}
+	}
+	
+	public void Display() {
+		System.out.println("Frecuencia actual: "+ frecuencia + " MHz");
+	}
+}
